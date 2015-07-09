e4a339c8c38cf73d1bbdd33a7788d554ffda5e55
Rohit Gurnani <rkgi10@gmail.com>
2015-07-09 19:04:11 +0530

    Initial Commit

---
+++ AlgoNinja/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AlgoNinja
+//
+//  Created by Rohit Gurnani on 09/07/15.
+//  Copyright (c) 2015 Sophomore Ventures. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
